89b52067fef8116d14dc8a3e47ee4b9c4ae9adf9
Raja Haseeb <rajahaseeb147@gmail.com>
2023-03-06 11:48:49 +0900

    Update .tmux.zsh

--- config_files/tmux/.tmux.zsh
+++ config_files/tmux/.tmux.zsh
@@ -12,33 +12,40 @@ alias tn='tmux new-session'
 # Lists all ongoing sessions
 alias tl='tmux list-sessions'
 
+# rename window
 alias tr='tmux rename-window'
 
+# pane title
+alias tp='tmux select-pane -T'
+
+# kill panes
+alias tk='tmux kill-pane -a -t'
+
 tsplit() {
-	if [ $1 -lt 4 ]; then
-		return;
-	fi
+        if [ $1 -lt 4 ]; then
+                return;
+        fi
 
-	if [ $1 -eq 4 ]; then
-		tmux split-window -h;
-		tmux split-window -v;
-		tmux select-pane -L;
-		tmux split-window -v;
-		return;
-	fi
+        if [ $1 -eq 4 ]; then
+                tmux split-window -h;
+                tmux split-window -v;
+                tmux select-pane -L;
+                tmux split-window -v;
+                return;
+        fi
 
-	log2_odd_even=$(($(echo "l($1)/l(2)" | bc -l | xargs printf '%.0f')%2))
+        log2_odd_even=$(($(echo "l($1)/l(2)" | bc -l | xargs printf '%.0f')%2))
 
-	if [ $log2_odd_even -eq 0 ]; then
-		tmux split-window -v;
-		tsplit $(($1/2));
-		tmux select-pane -U;
-		tmux select-pane -U;
-		tsplit $(($1/2));
-	else
-		tmux split-window -h;
-		tsplit $(($1/2));
-		tmux select-pane -L;
-		tsplit $(($1/2));
-	fi
+        if [ $log2_odd_even -eq 0 ]; then
+                tmux split-window -v;
+                tsplit $(($1/2));
+                tmux select-pane -U;
+                tmux select-pane -U;
+                tsplit $(($1/2));
+        else
+                tmux split-window -h;
+                tsplit $(($1/2));
+                tmux select-pane -L;
+                tsplit $(($1/2));
+        fi
 }
