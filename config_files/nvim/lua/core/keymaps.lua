cb56fc90ce24fabd23c3aa12e1ad010244627f4a
Raja Haseeb <rajahaseeb147@gmail.com>
2023-05-04 09:31:37 +0900

    Update keymaps.lua

--- config_files/nvim/lua/core/keymaps.lua
+++ config_files/nvim/lua/core/keymaps.lua
@@ -11,16 +11,16 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Remap ctrl-/ for vertical split
-vim.api.nvim_set_keymap('n', '<C-_>', ':vsplit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-_>", ":vsplit<CR>", { noremap = true, silent = true })
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -28,12 +28,11 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-
 -- grep remap --
-vim.api.nvim_set_keymap('n', '<C-g>', [[:vimgrep /input/ %:p]], {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>vg", [[:vimgrep /input/ %:p]], { noremap = true })
 
-vim.keymap.set("n", ">", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", ".", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", ",", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
@@ -47,22 +46,22 @@ vim.keymap.set("n", "<leader>n", "<cmd>:NvimTreeFindFileToggle<CR>", { silent = 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Remap Tab and Shift-Tab for indenting and unindenting
-vim.api.nvim_set_keymap('v', '<Tab>', '>gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.smart_tab()', { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap('i', '<S-Tab>', 'v:lua.smart_shift_tab()', { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("v", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.smart_tab()", { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.smart_shift_tab()", { noremap = true, silent = true, expr = true })
 -- Define the smart_tab() and smart_shift_tab() functions
 -- function _G.smart_tab()
 --     return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
 -- end
 function _G.smart_tab()
-    if vim.fn.mode() == 'i' then -- fixed the issue where it wrote <tab> in insert mode
-        return "\t"
-    else
-        return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
-    end
+	if vim.fn.mode() == "i" then -- fixed the issue where it wrote <tab> in insert mode
+		return "\t"
+	else
+		return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
+	end
 end
 
 function _G.smart_shift_tab()
-    return vim.fn.pumvisible() == 1 and "<C-p>" or "<S-Tab>"
+	return vim.fn.pumvisible() == 1 and "<C-p>" or "<S-Tab>"
 end
